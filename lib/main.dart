9f96c462d963cef1deb29c459668a3b2297fd34f
JHu2018 <47310666+JHu2018@users.noreply.github.com>
2020-02-16 14:43:03 -0500

    added info page

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Logistic Map',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Logistic Map'),
-          ),
-          body: Center(
-            child: PopulationSimulation(),
-          ),
-        ));
+      title: 'Logistic Map',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Logistic Map'),
+        actions: <Widget>[
+          RaisedButton(
+              onPressed: () {
+                print("HEFFDS");
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new InfoPage()));
+              },
+              child: Text("Info")),
+        ],
+      ),
+      body: Center(
+        child: PopulationSimulation(),
+      ),
+    );
+  }
+}
+
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("InfoPage"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(5),
+        children: <Widget>[
+          Text("x\u2099\u208A\u2081 = rx\u2099(1-x\u2099)", style: TextStyle(fontSize: 30)), 
+          Text("HELLO"), 
+          Text("HELLO")],
+      ),
+    );
   }
 }
