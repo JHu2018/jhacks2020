8ad2dcc8139764e03af9a042765905e002c6f3fe
jasmaa <jasonmaa3955@gmail.com>
2020-02-16 12:51:15 -0500

    Add playback slider, add number display

--- lib/population.dart
+++ lib/population.dart
@@ -1,83 +1,158 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart'; 
+import 'package:flutter/animation.dart';
 import 'package:quiver/async.dart';
+import 'package:intl/intl.dart';
 
 class PopulationSimulation extends StatefulWidget {
   @override
-  PopulationSimulationState createState() => PopulationSimulationState();
+  _PopulationSimulationState createState() => _PopulationSimulationState();
 }
 
-class PopulationSimulationState extends State<PopulationSimulation> {
-  final List<double> progress = <double>[0.20, 0.201, 0.209, 0.20, 0.20, 0.206, 0.208];
-  double r = 3.7;
-
+/// Population simulator
+class _PopulationSimulationState extends State<PopulationSimulation> {
+  final List<List<double>> _progressLog = <List<double>>[[]];
+  double _r = 3.8;
   bool _isRunning = false;
+  int _currIdx = 0;
+  final int _iterations = 20;
+  bool _isRunOnce = false;
+
+  void initState() {
+    super.initState();
+    buildStartProgress(0);
+  }
+
+  void buildStartProgress(x) {
+    final gen = Random();
+    final List<double> currProgress = <double>[];
+
+    for (var i = 0; i < 10; i++) {
+      currProgress.add(x + gen.nextDouble() * 0.01);
+    }
+
+    _progressLog.clear();
+    _progressLog.add(currProgress);
+  }
 
   void startTimer() {
     final int interval = 100;
-    final int iterations = 10;
 
     CountdownTimer countDownTimer = new CountdownTimer(
-      new Duration(milliseconds: iterations * interval),
+      new Duration(milliseconds: _iterations * interval),
       new Duration(milliseconds: interval),
     );
 
     setState(() {
       _isRunning = true;
+      buildStartProgress(0.7);
+      _currIdx = 0;
     });
 
-    var sub = countDownTimer.listen(null);
+    final sub = countDownTimer.listen(null);
     sub.onData((duration) {
       setState(() {
-        for (var i = 0; i < progress.length; i++) {
-          progress[i] = r * progress[i] * (1 - progress[i]);
+        var prevProgress = _progressLog[_progressLog.length - 1];
+        var currProgress = <double>[];
+        for (var i = 0; i < prevProgress.length; i++) {
+          currProgress.add(_r * prevProgress[i] * (1 - prevProgress[i]));
         }
+        _progressLog.add(currProgress);
+        _currIdx++;
       });
     });
 
     sub.onDone(() {
       setState(() {
         _isRunning = false;
+        _isRunOnce = true;
       });
       sub.cancel();
     });
   }
 
+  Widget renderControl() {
+    if (!_isRunning) {
+      return Column(
+        children: <Widget>[
+          _isRunOnce
+              ? Row(
+                  children: <Widget>[
+                    const Text('Playback:'),
+                    Expanded(
+                      child: Slider(
+                        value: _currIdx / _iterations,
+                        onChanged: (x) {
+                          setState(() {
+                            _currIdx = (x * _iterations).round();
+                          });
+                        },
+                      ),
+                    )
+                  ],
+                )
+              : Container(),
+          RaisedButton(
+            onPressed: () {
+              if (!_isRunning) startTimer();
+            },
+            child: const Text('Run Simulation'),
+          ),
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        PopulationBars(progress),
-        Text('$_isRunning'),
-        RaisedButton(
-          onPressed: () {
-            if(!_isRunning) startTimer();
-          },
-          child: const Text("Click me"),
-        ),
-      ],
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Column(
+        children: <Widget>[
+          _PopulationBars(_progressLog[_currIdx]),
+          renderControl(),
+        ],
+      ),
     );
   }
 }
 
-class PopulationBars extends StatelessWidget {
+/// Renders population bars
+class _PopulationBars extends StatelessWidget {
   final List<double> _progress;
 
-  PopulationBars(this._progress);
+  _PopulationBars(this._progress);
 
   @override
   Widget build(BuildContext context) {
+
     return Expanded(
       child: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
         itemCount: this._progress.length,
         itemBuilder: (context, i) {
+
+          final formatter = NumberFormat('00%');
+
           return Container(
             margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
-            child: LinearProgressIndicator(
-              value: this._progress[i].toDouble(),
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green[300]),
-              backgroundColor: Colors.grey[300],
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(right: 5.0),
+                  child: Text(formatter.format(this._progress[i])),
+                ),
+                Expanded(
+                  child: LinearProgressIndicator(
+                    value: this._progress[i].toDouble(),
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Colors.green[300]),
+                    backgroundColor: Colors.grey[300],
+                  ),
+                )
+              ],
             ),
           );
         },
